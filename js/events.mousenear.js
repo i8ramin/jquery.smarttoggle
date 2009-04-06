ea334253c33fea01f284f01498f9d95530635926
Ramin <i8ramin@gmail.com>
2009-04-05 22:06:11 -0400

    using updated mousenear event. a few styling changes

--- js/events.mousenear.js
+++ js/events.mousenear.js
@@ -10,42 +10,48 @@
  */
 (function($) {
 
-$.fn.extend({
-	mousenear: function(f, settings) {
+jQuery.event.special.mousenear = {
+	setup: function(data, namespaces) {
+		jQuery(document).bind('mousemove', { elem: this, settings: data }, jQuery.event.special.mousenear.handler);
+	},
+	teardown: function(namespaces) {
+		jQuery(document).unbind('mousemove', jQuery.event.special.mousenear.handler);
+	},
+	handler: function(event) {
+		var $elem = jQuery(event.data.elem),
+				settings = event.data.settings,
+				args = arguments,
+				pageX, pageY,
+				topTrigger, rightTrigger, bottomTrigger, leftTrigger;
+		
 		settings = $.extend({
 			trigger: 'top,right,bottom,left',
 			proximity: 50
 		}, settings || {});
 		
-		return this.each(function() {
-			var self = $(this),
-					pageX, pageY,
-					topTrigger, rightTrigger, bottomTrigger, leftTrigger;
-					
-			self.bind('mousenear', f, settings);
-			
-			$(document).mousemove(function(e) {
-				pageX = e.pageX; pageY = e.pageY;
-
-				topTrigger = Math.round(self.offset().top);
-				rightTrigger = Math.round(self.offset().left) + self.outerWidth();
-				bottomTrigger = Math.round(self.offset().top) + self.outerHeight();
-				leftTrigger = Math.round(self.offset().left);
-
-				var hotspots = { 
-					top: pageY > (topTrigger - settings.proximity) && pageY <= topTrigger && pageX >= leftTrigger && pageX <= rightTrigger,
-					right: pageX <= (rightTrigger + settings.proximity) && pageX > rightTrigger && pageY >= topTrigger && pageY <= bottomTrigger,
-					bottom: pageY <= (bottomTrigger + settings.proximity) && pageY > bottomTrigger && pageX >= leftTrigger && pageX <= rightTrigger,
-					left: pageX > (leftTrigger - settings.proximity) && pageX <= leftTrigger && pageY >= topTrigger && pageY <= bottomTrigger
-				};
-
-				$.each(settings.trigger.split(','), function(i, v) {
-					if(hotspots[v]) { f.apply(self, [e, settings]); return false; }
-				});
-			});
+		pageX = event.pageX; pageY = event.pageY;
+		
+		topTrigger = Math.round($elem.offset().top);
+		rightTrigger = Math.round($elem.offset().left) + $elem.outerWidth();
+		bottomTrigger = Math.round($elem.offset().top) + $elem.outerHeight();
+		leftTrigger = Math.round($elem.offset().left);
+		
+		var hotspots = { 
+			top: pageY > (topTrigger - settings.proximity) && pageY <= topTrigger && pageX >= leftTrigger && pageX <= rightTrigger,
+			right: pageX <= (rightTrigger + settings.proximity) && pageX > rightTrigger && pageY >= topTrigger && pageY <= bottomTrigger,
+			bottom: pageY <= (bottomTrigger + settings.proximity) && pageY > bottomTrigger && pageX >= leftTrigger && pageX <= rightTrigger,
+			left: pageX > (leftTrigger - settings.proximity) && pageX <= leftTrigger && pageY >= topTrigger && pageY <= bottomTrigger
+		};
+		
+		// set event type to "mousenear"
+		event.type = "mousenear";
+		
+		$.each(settings.trigger.split(','), function(i, v) {
+			// let jQuery handle the triggering of "mousenear" event handlers
+			if(hotspots[v]) { jQuery.event.handle.apply($elem[0], args); return false; }
 		});
 	}
-});
+};
 
 /*global jQuery */
 })(jQuery);
